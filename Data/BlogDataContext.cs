c429d7861dc4d0aeb06e3fd42c49664e9298f193
denisbao <denis.tpd@gmail.com>
2022-01-26 14:26:08 -0300

    Connection String

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -11,5 +11,13 @@ namespace Blog.Data
     public DbSet<User> Users { get; set; }
     public DbSet<Role> Roles { get; set; }
     public DbSet<Tag> Tags { get; set; }
+
+
+    // Definição da Conection String:
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+      options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$");
+    }
+
   }
 }
