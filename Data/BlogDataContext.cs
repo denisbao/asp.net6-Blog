4cdd6635ae50f3823a4473dab909691561f169e0
denisbao <denis.tpd@gmail.com>
2022-01-26 15:14:17 -0300

    Adicionado mapeamento ao DataContext

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -1,3 +1,4 @@
+using Blog.Data.Mappings;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,16 @@ namespace Blog.Data
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
       options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$");
+    }
+
+    // Configuração do Fluent Mapping:
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfiguration(new CategoryMap());
+      modelBuilder.ApplyConfiguration(new UserMap());
+      modelBuilder.ApplyConfiguration(new PostMap());
+      modelBuilder.ApplyConfiguration(new RoleMap());
+      modelBuilder.ApplyConfiguration(new TagMap());
     }
 
   }
