26cf4f1e2f9a8f3e4c07af3157d5f21cb886ac4d
denisbao <denis.tpd@gmail.com>
2022-01-27 16:23:56 -0300

    Adicionadas validações no ViewModel da Categoria

--- ViewModels/EditorCategoryViewModel.cs
+++ ViewModels/EditorCategoryViewModel.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Blog.ViewModels
 {
   public class EditorCategoryViewModel
   {
+    [Required(ErrorMessage = "O campo nome é obrigarório.")]
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "O campo deve ter entre 3 e 40 caracteres.")]
     public string Name { get; set; }
+
+
+    [Required(ErrorMessage = "O campo slug é obrigarório.")]
     public string Slug { get; set; }
 
   }
