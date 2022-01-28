30468f6acbaeb3e4e3cf7d4e61a9691cbcd88f23
denisbao <denis.tpd@gmail.com>
2022-01-28 10:31:41 -0300

    Definição inicial da JwtKey

---
+++ Configuration.cs
@@ -0,0 +1,9 @@
+namespace Blog
+{
+  public static class Configuration
+  {
+    // JSON Web Token (JWT):
+    public static string JwtKey { get; set; } = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+
+  }
+}
