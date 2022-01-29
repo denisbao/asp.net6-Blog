9d2e56fb8af37cab5a9ca528481ebae00c027a19
denisbao <denis.tpd@gmail.com>
2022-01-29 15:53:50 -0300

    Iniciando o AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+  public class AccountController : ControllerBase
+  {
+    [HttpPost("v1/login")]
+    public IActionResult Login(
+      [FromServices] TokenService _tokenService
+    )
+    {
+      var token = _tokenService.GenerateToken(null);
+      return Ok(token);
+    }
+  }
+}
