1b8117e5ef61ccbbe5c0853edc4c6e1efaf1199f
denisbao <denis.tpd@gmail.com>
2022-03-16 10:07:01 -0300

    Removendo o password do usuário do JSON

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Blog.Models
 {
   public class User
@@ -5,7 +7,10 @@ namespace Blog.Models
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+
+    [JsonIgnore]
     public string Password { get; set; }
+    
     public string Image { get; set; }
     public string Slug { get; set; }
     public string Bio { get; set; }
