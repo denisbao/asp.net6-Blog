b366878670706694bfad8f88eb21480ebc4885fa
denisbao <denis.tpd@gmail.com>
2022-03-17 09:36:37 -0300

    Adicionando paginação a listagem de posts

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,6 @@
 using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,25 +12,43 @@ public class PostController : ControllerBase
 {
   [HttpGet("v1/posts")]
   public async Task<IActionResult> GetAsync (
-    [FromServices] BlogDataContext context
+    [FromServices] BlogDataContext context,
+    [FromQuery] int page = 0,
+    [FromQuery] int pageSize = 10
   )
   {
-    var posts = await context
+    try
+    {
+      var countPosts = await context.Posts.AsNoTracking().CountAsync();
+      var posts = await context
       .Posts
       .AsNoTracking()
       .Include(x => x.Category)
       .Include(x => x.Author)
-      // .Select(x => new ListPostsViewModel
-      // {
-      //   Id = x.Id,
-      //   Title = x.Title,
-      //   Slug = x.Slug,
-      //   LastUpdateDate = x.LastUpdateDate,
-      //   Category = x.Category.Name,
-      //   Author = $"{x.Author.Name} ({x.Author.Email})"
-      // })
+      .Select(x => new ListPostsViewModel
+      {
+        Id = x.Id,
+        Title = x.Title,
+        Slug = x.Slug,
+        LastUpdateDate = x.LastUpdateDate,
+        Category = x.Category.Name,
+        Author = $"{x.Author.Name} ({x.Author.Email})"
+      })
+      .Skip(page * pageSize)
+      .Take(pageSize)
       .ToListAsync();
 
-    return Ok(posts);
+      return Ok(new ResultViewModel<dynamic>(new 
+      {
+        total = countPosts,
+        page,
+        pageSize,
+        posts
+      }));
+    }
+    catch (Exception ex)
+    {
+       return StatusCode(500, new ResultViewModel<List<Post>>("COD038: Falha interna no servidor"));
+    }
   }
 }
